fe0afa7b8affb39d0398481fee4c507bf5c8424f
Ariuhaaa <115851178+Ariuhaaa@users.noreply.github.com>
2023-03-09 21:21:40 +0800

    routess

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-const port = 8080;
+const port = 6600;
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const menuRouter = require("./routes/menu.route.js");
-const cateRouter = require("./routes/category.route.js");
-const productRouter = require("./routes/product.route.js");
-const userRouter = require("./routes/user.route.js");
-
-const fs = require("fs");
+const employeeRouter = require("./routes/employee.routes");
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", menuRouter);
-app.use("/api", cateRouter);
-app.use("/api", productRouter);
-app.use("/api", userRouter);
+app.use("/api", employeeRouter);
 
 app.get("/api", (req, res) => {
   res.json({ message: "Welcome Rest API" });
 });
 
-app.listen(port, () => console.log("Server is running"));
+app.listen(port, () => console.log("server is running on " + port));
